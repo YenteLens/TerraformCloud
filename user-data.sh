fa8618ae70639071671527716e30b4fc1cf3380a
YenteLens <lensyente@gmail.com>
2025-12-13 22:00:47 +0100

    update user data

--- user-data.sh
+++ user-data.sh
@@ -2,7 +2,7 @@
 sudo apt update -y 
 sudo apt-get update
 sudo apt install php libapache2-mod-php -y
-sudo apt-get install mysql-server php php-mysql
+sudo apt-get install mysql-server php php-mysql -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
 unzip awscliv2.zip
